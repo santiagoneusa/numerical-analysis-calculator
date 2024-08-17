954d0776e660cc9a6058c79add10d2d38ecb9519
Santiago Neusa <sneusar@gmail.com>
2024-08-16 19:54:38 -0500

    implement partials on templates

--- methods/views.py
+++ methods/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 def index(request):
     template_data = {'message': 'Welcome to the calculator app'}
-    return render(request, 'methods/index.html', {"template_data": template_data})
+    return render(request, 'home/index.html', {"template_data": template_data})
