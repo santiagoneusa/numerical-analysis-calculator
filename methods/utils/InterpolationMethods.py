68458e1f8f1c2411645c0fa71c8ec5fa1d3f3ecd
Santiago Neusa <sneusar@gmail.com>
2024-09-12 15:07:45 -0500

    imp: methods on utils

---
+++ methods/utils/InterpolationMethods.py
@@ -0,0 +1,26 @@
+class InterpolationMethods():
+    
+    @staticmethod
+    def vandermonde():
+        pass
+
+    @staticmethod
+    def newton_divided_difference():
+        pass
+
+    @staticmethod
+    def lagrange():
+        pass
+
+    @staticmethod
+    def spline_linear():
+        pass
+
+    @staticmethod
+    def spline_square():
+        pass
+
+    @staticmethod
+    def spline_cubic():
+        pass
+
---
+++ methods/utils/SystemsEquationMethods.py
@@ -0,0 +1,45 @@
+class SystemsEquationMethods():
+
+    @staticmethod
+    def gaussian_elimination_simple():
+        pass
+
+    @staticmethod
+    def gaussian_elimination_partial_pivot():
+        pass
+
+    @staticmethod
+    def gaussian_elimination_total_pivot():
+        pass
+
+    @staticmethod
+    def direct_factorization_lu_simple():
+        pass
+
+    @staticmethod
+    def direct_factorization_lu_partial():
+        pass
+
+    @staticmethod
+    def croult():
+        pass
+
+    @staticmethod
+    def doolittle():
+        pass
+
+    @staticmethod
+    def cholesky():
+        pass
+
+    @staticmethod
+    def jacobi():
+        pass
+
+    @staticmethod
+    def gauss_seidel():
+        pass
+
+    @staticmethod
+    def sor():
+        pass
