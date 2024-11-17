023619294676f52c7d6813ba0cfc6e5ec62654a9
Santiago Neusa <sneusar@gmail.com>
2024-11-16 21:07:52 -0500

    fix: bugs

--- methods/urls.py
+++ methods/urls.py
@@ -1,45 +1,21 @@
 from django.urls import path
-from .views import NonLinearEquationsViews, SystemsEquationsViews, InterpolationViews
+from methods.views import NonLinearEquationsViews, SystemsEquationsViews, InterpolationViews
 
 urlpatterns = [
     path("bisection", NonLinearEquationsViews.bisection, name="methods.bisection"),
-    path(
-        "fixed-point", NonLinearEquationsViews.fixed_point, name="methods.fixed_point"
-    ),
-    path(
-        "false-position",
-        NonLinearEquationsViews.false_position,
-        name="methods.false_position",
-    ),
-    path(
-        "newton-raphson",
-        NonLinearEquationsViews.newton_raphson,
-        name="methods.newton_raphson",
-    ),
+    path("fixed-point", NonLinearEquationsViews.fixed_point, name="methods.fixed_point"),
+    path("false-position", NonLinearEquationsViews.false_position, name="methods.false_position"),
+    path("newton-raphson", NonLinearEquationsViews.newton_raphson, name="methods.newton_raphson"),
     path("secant", NonLinearEquationsViews.secant, name="methods.secant"),
-    path(
-        "multiple-roots-v1",
-        NonLinearEquationsViews.multiple_roots_v1,
-        name="methods.multiple_roots_v1",
-    ),
-    path(
-        "multiple-roots-v2",
-        NonLinearEquationsViews.multiple_roots_v2,
-        name="methods.multiple_roots_v2",
-    ),
+    path("multiple-roots-v1", NonLinearEquationsViews.multiple_roots_v1, name="methods.multiple_roots_v1"),
+    path("multiple-roots-v2", NonLinearEquationsViews.multiple_roots_v2, name="methods.multiple_roots_v2"),
+
     path("jacobi", SystemsEquationsViews.jacobi, name="methods.jacobi"),
-    path(
-        "gauss-seidel", SystemsEquationsViews.gauss_seidel, name="methods.gauss_seidel"
-    ),
+    path("gauss-seidel", SystemsEquationsViews.gauss_seidel, name="methods.gauss_seidel"),
     path("sor", SystemsEquationsViews.sor, name="methods.sor"),
+
     path("vandermonde", InterpolationViews.vandermonde, name="methods.vandermonde"),
-    path(
-        "newton-divided-difference",
-        InterpolationViews.newton_divided_difference,
-        name="methods.newton_divided_difference",
-    ),
+    path("newton-divided-difference", InterpolationViews.newton_divided_difference, name="methods.newton_divided_difference"),
     path("lagrange", InterpolationViews.lagrange, name="methods.lagrange"),
-    path(
-        "spline-linear", InterpolationViews.spline_linear, name="methods.spline_linear"
-    ),
+    path("spline-linear", InterpolationViews.spline_linear, name="methods.spline_linear"),
 ]
--- methods/views/InterpolationViews.py
+++ methods/views/InterpolationViews.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from methods.utils.ResponseManager import ResponseManager
-from methods.utils.EquationsManager import MathEquationsManager
+from methods.utils.EquationsManager import EquationsManager
 from methods.methods.InterpolationMethods import InterpolationMethods
 from django.urls import reverse
 
